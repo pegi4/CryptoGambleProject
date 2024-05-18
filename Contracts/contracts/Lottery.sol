390641dbdd4ba3bc2c58dd9472fc3e7041779e6c
pegi4 <vito.senic@gmail.com>
2024-05-18 22:32:28 +0100

    Test for pickWinner() func.

--- Contracts/contracts/Lottery.sol
+++ Contracts/contracts/Lottery.sol
@@ -6,30 +6,36 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Lottery {
     IERC20 public token;
     address[] public players;
-    uint256 public constant MINIMUM_ENTRY_AMOUNT = 100 * 10 ** 18; // Adjust as needed
+    mapping(address => uint256) public playerStakes;
+    uint256 public constant MINIMUM_BALANCE = 100 * 10 ** 18;
 
     constructor(address _tokenAddress) {
         token = IERC20(_tokenAddress);
     }
 
-    function enterLottery() public {
-        require(token.balanceOf(msg.sender) >= MINIMUM_ENTRY_AMOUNT, "Insufficient token balance");
-        require(token.transferFrom(msg.sender, address(this), MINIMUM_ENTRY_AMOUNT), "Transfer failed");
-        players.push(msg.sender);
-
-        if (players.length == 5) {
-            pickWinner();
+    function enterLottery(uint256 stake) public {
+        require(token.balanceOf(msg.sender) >= MINIMUM_BALANCE, "You must own at least 100 tokens to enter the lottery");
+        require(stake > 0, "Stake must be greater than zero");
+        require(token.transferFrom(msg.sender, address(this), stake), "Transfer failed");
+        if (playerStakes[msg.sender] == 0) {
+            players.push(msg.sender);
         }
+        playerStakes[msg.sender] += stake;
     }
 
-    function pickWinner() internal {
+    function pickWinner() public {
         require(players.length == 5, "Not enough players");
 
-        uint256 winnerIndex = block.timestamp % players.length;
+        uint256 random = uint256(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players)));
+        uint256 winnerIndex = random % players.length;
         address winner = players[winnerIndex];
 
         uint256 prize = token.balanceOf(address(this));
         token.transfer(winner, prize);
+
+        for (uint256 i = 0; i < players.length; i++) {
+            playerStakes[players[i]] = 0;
+        }
 
         delete players;
     }
--- Contracts/test/MyToken.test.js
+++ Contracts/test/MyToken.test.js
@@ -9,7 +9,7 @@ let chai;
 })();
 
 contract("GambleToken", (accounts) => {
-  const [deployer, user1, user2] = accounts;
+  const [deployer, user1, user2, user3, user4, user5] = accounts;
   let token;
   let lottery;
 
@@ -24,19 +24,75 @@ contract("GambleToken", (accounts) => {
   });
 
   it("should transfer tokens correctly", async () => {
-    await token.transfer(user1, web3.utils.toWei('150', 'ether'), { from: deployer });
+    await token.transfer(user1, web3.utils.toWei('200', 'ether'), { from: deployer });
     const balance1 = await token.balanceOf(user1);
-    expect(balance1.toString()).to.equal(web3.utils.toWei('150', 'ether'));
+    expect(balance1.toString()).to.equal(web3.utils.toWei('200', 'ether'));
 
-    await token.transfer(user2, web3.utils.toWei('50', 'ether'), { from: user1 });
+    await token.transfer(user2, web3.utils.toWei('100', 'ether'), { from: user1 });
     const balance2 = await token.balanceOf(user2);
-    expect(balance2.toString()).to.equal(web3.utils.toWei('50', 'ether'));
+    expect(balance2.toString()).to.equal(web3.utils.toWei('100', 'ether'));
+
+    await token.transfer(user3, web3.utils.toWei('100', 'ether'), { from: deployer });
+    const balance3 = await token.balanceOf(user3);
+    expect(balance3.toString()).to.equal(web3.utils.toWei('100', 'ether'));
+
+    await token.transfer(user4, web3.utils.toWei('100', 'ether'), { from: deployer });
+    const balance4 = await token.balanceOf(user4);
+    expect(balance4.toString()).to.equal(web3.utils.toWei('100', 'ether'));
+
+    await token.transfer(user5, web3.utils.toWei('100', 'ether'), { from: deployer });
+    const balance5 = await token.balanceOf(user5);
+    expect(balance5.toString()).to.equal(web3.utils.toWei('100', 'ether'));
   });
 
   it("should allow entry into lottery", async () => {
     await token.approve(lottery.address, web3.utils.toWei('100', 'ether'), { from: user1 });
-    await lottery.enterLottery({ from: user1 });
+    await lottery.enterLottery(web3.utils.toWei('100', 'ether'), { from: user1 });
     const balance1 = await token.balanceOf(user1);
     expect(balance1.toString()).to.equal(web3.utils.toWei('0', 'ether'));
+
+    await token.approve(lottery.address, web3.utils.toWei('100', 'ether'), { from: user2 });
+    await lottery.enterLottery(web3.utils.toWei('100', 'ether'), { from: user2 });
+    const balance2 = await token.balanceOf(user2);
+    expect(balance2.toString()).to.equal(web3.utils.toWei('0', 'ether'));
+
+    await token.approve(lottery.address, web3.utils.toWei('100', 'ether'), { from: user3 });
+    await lottery.enterLottery(web3.utils.toWei('100', 'ether'), { from: user3 });
+    const balance3 = await token.balanceOf(user3);
+    expect(balance3.toString()).to.equal(web3.utils.toWei('0', 'ether'));
+
+    await token.approve(lottery.address, web3.utils.toWei('100', 'ether'), { from: user4 });
+    await lottery.enterLottery(web3.utils.toWei('100', 'ether'), { from: user4 });
+    const balance4 = await token.balanceOf(user4);
+    expect(balance4.toString()).to.equal(web3.utils.toWei('0', 'ether'));
+
+    await token.approve(lottery.address, web3.utils.toWei('100', 'ether'), { from: user5 });
+    await lottery.enterLottery(web3.utils.toWei('100', 'ether'), { from: user5 });
+    const balance5 = await token.balanceOf(user5);
+    expect(balance5.toString()).to.equal(web3.utils.toWei('0', 'ether'));
+  });
+
+  it("should pick a winner correctly when five players are entered", async () => {
+    const initialBalances = await Promise.all(accounts.slice(1, 6).map(async (account) => {
+      return {
+        account,
+        balance: await token.balanceOf(account).then(balance => balance.toString())
+      };
+    }));
+
+    // Call pickWinner
+    await lottery.pickWinner({ from: deployer });
+
+    const finalBalances = await Promise.all(accounts.slice(1, 6).map(async (account) => {
+      return {
+        account,
+        balance: await token.balanceOf(account).then(balance => balance.toString())
+      };
+    }));
+
+    const winner = finalBalances.find((final, index) => final.balance !== initialBalances[index].balance);
+    const prize = web3.utils.toWei('500', 'ether'); // 5 players with 100 tokens each
+    expect(winner).to.exist;
+    expect(winner.balance).to.equal(prize);
   });
 });
